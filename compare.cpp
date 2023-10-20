f41d30d99bd1ee9b578b8950bb1d95030e87228e
aditya-jnu <29adityamishra@gmail.com>
2023-10-20 23:39:52 +0530

    under learning

---
+++ compare.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if(a>b)
+    cout<<"a greater than b";
+    else if(a=b)
+    cout<<" Both are equal";
+    else
+    cout<<"b greater than a";
+}
